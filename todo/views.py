0787f4c7af6c410297ca027e1f60a210c64a98b2
kc <kc@KCEKALI.lab>
2018-12-25 16:11:04 +0100

    project changes

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def todoView(request):
-    # return HttpResponse('Hello this is a todo app')
-    return render(request, 'todo.html')
+# =============================================================================
+# Old version of todoView to show the content of todo.html template only
+# def todoView(request):
+#     # return HttpResponse('Hello this is a todo app')
+#     return render(request, 'todo.html')
+# =============================================================================
 
+
+from .models import todoItem
+
+def todoView(request):
+    # This function shows not just the html template, but also interacts with the sqlite DB
+    return render(request, 'todo.html', {'all_items': todoItem.objects.all()})
 # Create your views here.
